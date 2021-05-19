50cdfa57d5c3d9549c0216d24389a2145c6ae4a0
Stephanie Lewis <stephanie.lewis.art@gmail.com>
2021-05-19 21:44:24 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,4 @@ colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow(colors)
   puts "the first color of the rainbow is #{colors[0]}"
   puts "the second color of the rainbow is #{colors[1]}"
+end
